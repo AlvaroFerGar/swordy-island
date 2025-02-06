ed7fa07c82c2a0333b08ef5fcd97529cccb01d1f
AlvaroFerGar <alvarofergar95@gmail.com>
2025-02-06 12:11:01 +0100

    Separación en distintos archivos y eliminación de enemigos

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import Box from "./box.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -19,98 +20,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-
-class Box extends THREE.Mesh {
-  constructor({
-    width,
-    height,
-    depth,
-    color = "#00ff00",
-    velocity = {
-      x: 0,
-      y: 0,
-      z: 0,
-    },
-    position = {
-      x: 0,
-      y: 0,
-      z: 0,
-    },
-    zAcceleration = false,
-  }) {
-    super(
-      new THREE.BoxGeometry(width, height, depth),
-      new THREE.MeshStandardMaterial({ color })
-    );
-
-    this.width = width;
-    this.height = height;
-    this.depth = depth;
-
-    this.position.set(position.x, position.y, position.z);
-
-    this.right = this.position.x + this.width / 2;
-    this.left = this.position.x - this.width / 2;
-
-    this.bottom = this.position.y - this.height / 2;
-    this.top = this.position.y + this.height / 2;
-
-    this.front = this.position.z + this.depth / 2;
-    this.back = this.position.z - this.depth / 2;
-
-    this.velocity = velocity;
-    this.gravity = -0.002;
-
-    this.zAcceleration = zAcceleration;
-  }
-
-  updateSides() {
-    this.right = this.position.x + this.width / 2;
-    this.left = this.position.x - this.width / 2;
-
-    this.bottom = this.position.y - this.height / 2;
-    this.top = this.position.y + this.height / 2;
-
-    this.front = this.position.z + this.depth / 2;
-    this.back = this.position.z - this.depth / 2;
-  }
-
-  update(ground) {
-    this.updateSides();
-
-    if (this.zAcceleration) this.velocity.z += 0.0003;
-
-    this.position.x += this.velocity.x;
-    this.position.z += this.velocity.z;
-
-    this.applyGravity(ground);
-  }
-
-  applyGravity(ground) {
-    this.velocity.y += this.gravity;
-
-    // this is where we hit the ground
-    if (
-      boxCollision({
-        box1: this,
-        box2: ground,
-      })
-    ) {
-      const friction = 0.5;
-      this.velocity.y *= friction;
-      this.velocity.y = -this.velocity.y;
-    } else this.position.y += this.velocity.y;
-  }
-}
-
-function boxCollision({ box1, box2 }) {
-  const xCollision = box1.right >= box2.left && box1.left <= box2.right;
-  const yCollision =
-    box1.bottom + box1.velocity.y <= box2.top && box1.top >= box2.bottom;
-  const zCollision = box1.front >= box2.back && box1.back <= box2.front;
-
-  return xCollision && yCollision && zCollision;
-}
 
 const cube = new Box({
   width: 1,
@@ -149,8 +58,8 @@ scene.add(light);
 scene.add(new THREE.AmbientLight(0xffffff, 0.5));
 
 camera.position.z = 5;
-console.log(ground.top);
-console.log(cube.bottom);
+//console.log(ground.top);
+//console.log(cube.bottom);
 
 const keys = {
   a: {
@@ -168,6 +77,8 @@ const keys = {
 };
 
 window.addEventListener("keydown", (event) => {
+
+  console.log("manolo")
   switch (event.code) {
     case "KeyA":
       keys.a.pressed = true;
@@ -233,31 +144,6 @@ function animate() {
       cancelAnimationFrame(animationId);
     }
   });
-
-  if (frames % spawnRate === 0) {
-    if (spawnRate > 20) spawnRate -= 20;
-
-    const enemy = new Box({
-      width: 1,
-      height: 1,
-      depth: 1,
-      position: {
-        x: (Math.random() - 0.5) * 10,
-        y: 0,
-        z: -20,
-      },
-      velocity: {
-        x: 0,
-        y: 0,
-        z: 0.005,
-      },
-      color: "red",
-      zAcceleration: true,
-    });
-    enemy.castShadow = true;
-    scene.add(enemy);
-    enemies.push(enemy);
-  }
 
   frames++;
   // cube.position.y += -0.01
